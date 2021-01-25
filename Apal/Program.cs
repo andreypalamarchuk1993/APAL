77255b768269768e34e0e955654608b5e675881b
Andrey <a0933023758@gmail.com>
2021-01-25 19:15:39 +0200

    Start

---
+++ Apal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Apal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
